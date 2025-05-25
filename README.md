378430a5e94287fac7eace4836fffd1fc19e7e2b
Breathe97 <704340916@qq.com>
2025-05-25 16:29:57 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# pr-condition-queue
-js函数的条件队列管理。
+js 函数的任务队列管理。
+
+## 立即开始
+
+### 安装
+
+```bash
+npm i pr-task-queue
+```
+
+### 引入
+
+```js
+// 按需引入
+
+// 或全量引入
+import { PrTaskQueue } from 'pr-task-queue'
+```
+
+#### 创建队列
+
+```js
+const taskQueue = new PrTaskQueue(['login', 'isAdmin']) // 创建一个队列 队列中含有 是否登录 是否为管理员
+```
+
+#### 设置条件
+
+```js
+taskQueue.setCondition('login', true) // 已登录
+```
+
+#### 添加任务
+
+```js
+const func = () => {
+  console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;', `------->Breathe: 已登录`)
+}
+const taskKey = taskQueue.addTask(func, ['login'], { describe: '这是登录后才执行的函数' })
+```
+
+#### 清理任务
+
+```js
+taskQueue.clear(['taskKey']) // 清理指定任务
+taskQueue.clear() // 清理所有任务
+```
+
+#### 查询所有条件当前状态
+
+- 一般用不上 当任务没有安装预期执行可调用查看
+
+```js
+const res = taskQueue.getConditions()
+```
+
+#### 查询所有待执行任务
+
+- 一般用不上 当任务没有安装预期执行可调用查看
+
+```js
+const res = taskQueue.getTasks()
+```
---
+++ src/PrTaskQueue.ts
@@ -0,0 +1,130 @@
+export class PrTaskQueue<T extends string> {
+  #conditionMap = new Map<T, boolean>() // 条件
+
+  #tasks: Array<{ key: string; func: Function; conditionKeys: T[]; describe: string }> = [] // 待执行函数
+
+  index // 任务 index
+
+  /**
+   * 初始化条件
+   * @example const taskQueue = new PrTaskQueue(['login','isAdmin'])
+   * @param conditionKeys 条件keys string[]
+   */
+  constructor(conditionKeys: T[]) {
+    this.index = 0
+    // 设置所有条件为false
+    for (const conditionKey of conditionKeys) {
+      this.#conditionMap.set(conditionKey, false)
+    }
+  }
+
+  /**
+   * 设置条件
+   * @param conditionKey 条件名
+   * @param accord 条件状态
+   */
+  setCondition = (conditionKey: T, accord: boolean) => {
+    this.#conditionMap.set(conditionKey, accord)
+    if (accord) {
+      this.#checkExecute()
+    }
+  }
+
+  /**
+   * 添加待执行任务
+   * @param func 待执行任务
+   * @param conditionKeys 依赖条件 （所有条件符合时才执行该任务）
+   * @param describe 描述信息
+   */
+  addTask = (func: Function, conditionKeys: T[], options: { key?: string; describe?: string } = {}) => {
+    const _options = { key: '', describe: '', ...options }
+
+    let { key, describe } = _options
+    if (!key) {
+      key = `${this.index++}`
+    } else {
+      this.clear([key]) // 如果是自定义key 则需要查询是否已存在 并删除该任务
+    }
+
+    // 添加当前任务
+    {
+      const task = { key, func, conditionKeys, describe }
+      this.#tasks.unshift(task)
+    }
+    this.#checkExecute() // 检查和任务队列中可执行任务
+  }
+
+  /**
+   * 清理任务
+   * @param taskKey 任务keys
+   */
+  clear = (taskKeys: string[] = []) => {
+    // 删除所有
+    if (taskKeys.length === 0) {
+      this.#tasks = []
+      return
+    }
+
+    for (const taskKey of taskKeys) {
+      const index = this.#tasks.findIndex((item) => item.key === taskKey)
+      if (index !== -1) {
+        this.#tasks.splice(index, 1) // 移除改任务
+      }
+    }
+  }
+
+  /**
+   * 查询所有条件当前状态
+   */
+  getConditions = () => {
+    const obj = Object.fromEntries(this.#conditionMap)
+    return obj
+  }
+
+  /**
+   * 查询所有待执行任务
+   */
+  getTasks = () => {
+    const arr = []
+    for (const task of this.#tasks) {
+      const { key, describe, conditionKeys } = task
+      arr.push({ key, describe, conditionKeys })
+    }
+    return arr
+  }
+
+  /**
+   * 检查是否符合条件
+   * @param conditionKeys
+   */
+  #checkCondition = (conditionKeys: T[]) => {
+    let accord = true // 默认符合
+    for (const conditionKey of conditionKeys) {
+      const _accord = this.#conditionMap.get(conditionKey)
+      // 只有有一个条件不符合 直接跳出判断不符合
+      if (_accord === false) {
+        accord = false
+        break
+      }
+    }
+    return accord
+  }
+
+  /**
+   * 检查所有函数并执行
+   */
+  #checkExecute = () => {
+    const length = this.#tasks.length
+    for (let i = length; i > 0; i--) {
+      const index = i - 1
+      const item = this.#tasks[index]
+      const { conditionKeys, func } = item
+      const accord = this.#checkCondition(conditionKeys)
+      // 符合条件 执行函数并移除函数记录
+      if (accord) {
+        func()
+        this.#tasks.splice(index, 1)
+      }
+    }
+  }
+}
